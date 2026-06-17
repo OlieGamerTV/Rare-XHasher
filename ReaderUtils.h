c4b633639a0bc7fa0bcbd6fb683fda00bda89b55
OlieGamerTV <48225845+OlieGamerTV@users.noreply.github.com>
2026-06-17 15:46:58 +0100

    PDZ, Kameo and Loctext hash support
    Added the hashing code that generates the filenames used in Perfect Dark Zero (.pkg) and Kameo: Elements of Power (.lvl files), along with the hashing code for the localization text files.
    
    Additionally, spruced up the printout message a bit for when no options are present.

--- ReaderUtils.h
+++ ReaderUtils.h
@@ -16,3 +16,12 @@ static int flipEndian(int val) {
 static unsigned int flipEndian(unsigned int val) {
 	return ((0xFF000000 & val) >> 24) | ((0x00FF0000 & val) >> 8) | ((0x0000FF00 & val) << 8) | ((0x000000FF & val) << 24);
 }
+
+/// <summary>
+/// Flips the endian of the inputted value.
+/// </summary>
+/// <param name="val"></param>
+/// <returns><paramref name="val"/>, opposite of the current endian.</returns>
+static unsigned long long flipEndian(unsigned long long val) {
+	return ((0xFF00000000000000 & val) >> 56) | ((0x00FF000000000000 & val) >> 40) | ((0x0000FF0000000000 & val) >> 24) | ((0x000000FF00000000 & val) >> 8) | ((0x00000000ff000000 & val) << 8) | ((0x0000000000ff0000 & val) << 24) | ((0x000000000000ff00 & val) << 40) | ((0x00000000000000ff & val) << 56);
+}
--- rarehasher.cpp
+++ rarehasher.cpp
@@ -1,6 +1,8 @@
 // rarehasher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+
 #include <string.h>
+#include <typeinfo>
 #include <iostream>
 #include "AssetCommons.h"
 
@@ -8,23 +10,41 @@ int main(int argc, char* argv[])
 {
 	if (argc == 1) {
 		
-		printf("Usage: rarehasher -base/-typed [-verbose] <inFile>\n");
-		printf("       -base        -> Calculates a hash from a provided string using the algorithm for Grabbed by the Ghoulies and Viva Pinata.\n");
-		printf("       -typed       -> Calculates a hash from a provided string using the algorithm for Banjo-Kazooie: Nuts & Bolts and Kinect Sports.\n");
+		printf("Usage: rarehasher <hashOption> [-verbose] <inFile>\n");
+		printf("Base Options:\n");
+		printf("       -base        -> Calculates a hash using the algorithm for Grabbed by the Ghoulies and Viva Pinata.\n");
+		printf("       -dol         -> Calculates a hash using the algorithm for Kameo: Elements of Power.\n");
+		printf("       -dark        -> Calculates a hash using the algorithm for Perfect Dark Zero.\n");
+		printf("       -typed       -> Calculates a hash using the algorithm for Banjo-Kazooie: Nuts & Bolts and Kinect Sports.\n");
 		printf("\n");
+		printf("Misc. Options:\n");
+		printf("       -locHash     -> Calculates a hash used for the R1 localization text banks.\n");
 		printf("       -verbose     -> Optional parameter, prints out extra information when generating a hash.\n");
+
+		return 0;
+	}
+
+	if (strcmp(argv[1], "-locHash") == 0) {
+		if (argc < 3) {
+			std::cout << "No inFile was provided.\n";
+			return -1;
+		}
+
+		unsigned short hash = locHashElfHash16(argv[2]);
+
+		std::cout << "LOC Hash : " << hash << "\n";
 
 		return 0;
 	}
 
 	if (strcmp(argv[1], "-base") == 0) {
 		if (argc < 3) {
-			std::cout << "No inFile was provided.";
+			std::cout << "No inFile was provided.\n";
 			return -1;
 		}
 
 		if (strcmp(argv[2], "-verbose") == 0 && argc < 4) {
-			std::cout << "No inFile was provided.";
+			std::cout << "No inFile was provided.\n";
 			return -1;
 		}
 		if (strcmp(argv[2], "-verbose") == 0 && argc >= 4) {
@@ -37,14 +57,81 @@ int main(int argc, char* argv[])
 
 		return 0;
 	}
-	else if (strcmp(argv[1], "-typed") == 0) {
+	else if (strcmp(argv[1], "-dol") == 0) {
 		if (argc < 3) {
-			std::cout << "No inFile was provided.";
+			std::cout << "No inFile was provided.\n";
 			return -1;
 		}
 
 		if (strcmp(argv[2], "-verbose") == 0 && argc < 4) {
-			std::cout << "No inFile was provided.";
+			std::cout << "No inFile was provided.\n";
+			return -1;
+		}
+		if (strcmp(argv[2], "-verbose") == 0 && argc >= 4) {
+			unsigned int hash = assetIdGetHash_Dolphin(argv[3], true);
+
+			return 0;
+		}
+
+		unsigned int hash = assetIdGetHash_Dolphin(argv[2]);
+
+		printf("Relevant Structures:\n");
+		printf("                    Uberroot\\%08X.lvl\n", hash);
+
+		return 0;
+	}
+	else if (strcmp(argv[1], "-dark") == 0) {
+		if (argc < 3) {
+			std::cout << "No inFile was provided.\n";
+			return -1;
+		}
+
+		if (strcmp(argv[2], "-verbose") == 0 && argc < 4) {
+			std::cout << "No inFile was provided.\n";
+			return -1;
+		}
+		if (strcmp(argv[2], "-verbose") == 0 && argc >= 4) {
+			unsigned int hash = assetIdGetHash_Dark(argv[3], true);
+
+			return 0;
+		}
+
+		unsigned int hash = assetIdGetHash_Dark(argv[2]);
+
+		printf("Relevant Structures:\n");
+		printf("                    Uberroot\\PackageData\\%d.pkg\n", hash);
+
+		return 0;
+	}
+	else if (strcmp(argv[1], "-base_ext") == 0) {
+		std::cout << "WARNING: INCOMPLETE.\n";
+		if (argc < 3) {
+			std::cout << "No inFile was provided.\n";
+			return -1;
+		}
+
+		if (strcmp(argv[2], "-verbose") == 0 && argc < 4) {
+			std::cout << "No inFile was provided.\n";
+			return -1;
+		}
+		if (strcmp(argv[2], "-verbose") == 0 && argc >= 4) {
+			unsigned int hash = assetIdGetHash_BaseExt(argv[3], true);
+
+			return 0;
+		}
+
+		unsigned int hash = assetIdGetHash_BaseExt(argv[2]);
+
+		return 0;
+	}
+	else if (strcmp(argv[1], "-typed") == 0) {
+		if (argc < 3) {
+			std::cout << "No inFile was provided.\n";
+			return -1;
+		}
+
+		if (strcmp(argv[2], "-verbose") == 0 && argc < 4) {
+			std::cout << "No inFile was provided.\n";
 			return -1;
 		}
 		if (strcmp(argv[2], "-verbose") == 0 && argc >= 4) {
@@ -54,6 +141,16 @@ int main(int argc, char* argv[])
 		}
 
 		unsigned int hash = assetIdGetHash_Typed(argv[2]);
+
+		if (hash >> 24 == 0x4F || hash >> 24 == 0x50) {
+			printf("Relevant Structures (N&B):\n");
+			printf("-       Uberroot\\Bundle\\%02x\\%06x\n", hash >> 24, hash & 0x00FFFFFF);
+		}
+
+		if (hash >> 24 == 0x11 || hash >> 24 == 0x36) {
+			printf("Relevant Structures (N&B):\n");
+			printf("-       Uberroot\\Debug\\%02x\\%02x\\%02x\\%02x\n", hash >> 24, (hash >> 16) & 0xFF, (hash >> 8) & 0xFF, hash & 0xFF);
+		}
 
 		return 0;
 	}
